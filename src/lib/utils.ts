0245ee23168664206ac311b596d35608b91e37cb
Miguel Piedrafita <github@miguelpiedrafita.com>
2022-08-11 14:54:31 -0700

    fix return

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,5 +6,5 @@ export const shortAddr = hash => `${hash.slice(0, 5)}...${hash.slice(-4)}`
 export const decodeProof = (proof: ABIEncodedValue | null) => {
 	if (!proof) return
 
-	abi.decode(['uint256[8]'], proof as string)[0]
+	return abi.decode(['uint256[8]'], proof as string)[0]
 }
